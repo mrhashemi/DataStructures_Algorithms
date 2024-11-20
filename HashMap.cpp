60da9ee4577e955e7add7e5503dee28df69f365b
mrhashemi <56388210+mrhashemi@users.noreply.github.com>
2024-11-20 09:09:14 +0100

    Add files via upload

---
+++ HashMap.cpp
@@ -0,0 +1,75 @@
+#include <cstdio>
+
+template <class T> const T& max (const T& a, const T& b) {
+  return (a<b)?b:a;
+}
+
+class MyHashMap {
+public:
+    int* keys;
+    double* values;
+    int size;
+    //int* collisions;
+    int last_filled;
+    
+    MyHashMap() {
+        size = 1000000;
+        keys = new int[size]();
+        values = new double[size]();
+        last_filled = 0;
+        for (int i=0; i<size; i++){
+            values[i] = -1.0;
+        }
+    }
+
+    ~MyHashMap() {
+        delete[] keys;
+        delete[] values;
+    }
+    
+    void add(int key, double value) {
+        int index = key % size;
+        keys[index] = key;
+        values[index] = value;
+      	if (last_filled < index){
+        	last_filled = index;
+        }
+    }
+    
+    void remove(int key) {
+        int index = key % size;
+        if (keys[index] > 0){
+            values[index] = -1.0;
+        }
+        else{
+            printf("wrong key! \n");
+        }
+    }
+    
+    double get(int key) {
+        int index = key % size;
+        return values[index];
+    }
+};
+
+int main()
+{
+    MyHashMap* myobj = new MyHashMap();
+    myobj->add(1,6);
+    myobj->add(2,7);
+    myobj->add(4,3);
+    myobj->add(1,2);
+    myobj->add(1,16.5);
+    myobj->add(5,123);
+    myobj->add(3,0.1);
+    myobj->remove(4);
+    myobj->remove(100);
+
+    for (int i=1; i <= myobj->last_filled; i++){ //
+        printf("%f\n", myobj->get(i));
+    }
+
+    delete myobj;
+    
+    return 1;
+}
